90f2647ca45691cacb2723fefcdcbeccd5d6e00a
Sheri Lu <sherisherilu@gmail.com>
2023-04-10 11:56:08 -0700

    Edit schema

--- skeleton/prisma/migrations/20230404201944_init/migration.sql
+++ skeleton/prisma/migrations/20230404201944_init/migration.sql
@@ -10,7 +10,6 @@ CREATE TABLE "User" (
 -- CreateTable
 CREATE TABLE "Bingo_Board" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
     "squares" INTEGER NOT NULL,
     "isCompleted" BOOLEAN NOT NULL DEFAULT false,
     "difficultyLevel" INTEGER NOT NULL,
@@ -21,6 +20,7 @@ CREATE TABLE "Bingo_Board" (
 CREATE TABLE "Bingo_Square" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "position" INTEGER NOT NULL,
+    "isCompleted" BOOLEAN NOT NULL DEFAULT false,
     CONSTRAINT "Bingo_Square_bingoBoardId_fkey" FOREIGN KEY ("bingoBoardId") REFERENCES "Bingo_Board" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
     CONSTRAINT "Exercise_exerciseId_fkey" FOREIGN KEY ("exerciseId") REFERENCES "Exercise" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -30,7 +30,6 @@ CREATE TABLE "Exercise" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "title" TEXT NOT NULL,
     "description" TEXT,
-    "isCompleted" BOOLEAN NOT NULL DEFAULT false,
     "authorId" INTEGER NOT NULL
 );
 
