b8e0c27a560029675a892b0f9d618ba4f0eebe4f
Shafaq Saud <shafaq.saud@gmail.com>
2023-04-11 17:13:32 -0400

    Create Bingoboard

--- skeleton/pages/play/[difficulty].js
+++ skeleton/pages/play/[difficulty].js
@@ -1,16 +1,10 @@
-import Board from '@/modules/bingo';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from '@/styles/Home.module.css';
-import { PrismaClient } from '@prisma/client';
+import BingoBoard from '@/components/BingoBoard';
 
-const inter = Inter({ subsets: ['latin'] });
-
-export default function BingoBoard({ blogs }) {
+export default function DifficultyPage() {
   return (
     <div>
-      <Board></Board>
-
+      <h1>Difficulty Page</h1>
+      <BingoBoard />
     </div>
   );
 }
