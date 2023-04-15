66cb9cdd30573c79c04961e50d22c1538a4ba8b8
Richard Miao <richardmiao1645@gmail.com>
2023-04-14 17:14:37 -0700

    Create bingo_squares seed file

---
+++ skeleton/prisma/seeds/bingo_squares.ts
@@ -0,0 +1,196 @@
+import { Bingo_Square } from "@prisma/client";
+
+const bingo_squares: Bingo_Square[] = [
+  {
+    "id": 1,
+    "bingoBoardId": 1,
+    "position": 1,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 2,
+    "bingoBoardId": 1,
+    "position": 2,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 3,
+    "bingoBoardId": 1,
+    "position": 3,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 4,
+    "bingoBoardId": 1,
+    "position": 4,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 5,
+    "bingoBoardId": 1,
+    "position": 5,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 6,
+    "bingoBoardId": 1,
+    "position": 6,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 7,
+    "bingoBoardId": 1,
+    "position": 7,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 8,
+    "bingoBoardId": 1,
+    "position": 8,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 9,
+    "bingoBoardId": 1,
+    "position": 9,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 10,
+    "bingoBoardId": 1,
+    "position": 10,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 11,
+    "bingoBoardId": 1,
+    "position": 11,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 12,
+    "bingoBoardId": 1,
+    "position": 12,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 13,
+    "bingoBoardId": 1,
+    "position": 13,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 14,
+    "bingoBoardId": 1,
+    "position": 14,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 15,
+    "bingoBoardId": 1,
+    "position": 15,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 16,
+    "bingoBoardId": 1,
+    "position": 16,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 17,
+    "bingoBoardId": 1,
+    "position": 17,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 18,
+    "bingoBoardId": 1,
+    "position": 18,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 19,
+    "bingoBoardId": 1,
+    "position": 19,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 20,
+    "bingoBoardId": 1,
+    "position": 20,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 21,
+    "bingoBoardId": 1,
+    "position": 21,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 22,
+    "bingoBoardId": 1,
+    "position": 22,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 23,
+    "bingoBoardId": 1,
+    "position": 23,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  },
+  {
+    "id": 24,
+    "bingoBoardId": 1,
+    "position": 24,
+    "exerciseId": null,
+    "isCompleted": false,
+    "inProgress": false
+  }
+];
