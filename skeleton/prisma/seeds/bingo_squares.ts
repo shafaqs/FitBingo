459cf6efba87232d300eb36c7bc75e01f6788c70
Sheri Lu <sherisherilu@gmail.com>
2023-04-19 16:14:40 -0700

    Add more seeds

--- skeleton/prisma/seeds/bingo_squares.ts
+++ skeleton/prisma/seeds/bingo_squares.ts
@@ -1,3 +1,4 @@
+import { formLabelClasses } from "@mui/material";
 import { Bingo_Square } from "@prisma/client";
 
 const bingo_squares: Bingo_Square[] = [
@@ -240,6 +241,246 @@ const bingo_squares: Bingo_Square[] = [
     "inProgress": false,
     "createAt": new Date("2023-04-26"),
     "endAt": new Date("2023-04-26")
+  },
+  {
+    "id": 25,
+    "bingoBoardId": 2,
+    "position": 1,
+    "exerciseId": 1,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-01-10"),
+    "endAt": new Date("2023-01-10"),
+  },
+  {
+    "id": 26,
+    "bingoBoardId": 2,
+    "position": 2,
+    "exerciseId": 2,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-01-11"),
+    "endAt": new Date("2023-01-11")
+  },
+  {
+    "id": 27,
+    "bingoBoardId": 2,
+    "position": 3,
+    "exerciseId": 3,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-01-12"),
+    "endAt": new Date("2023-01-12")
+  },
+  {
+    "id": 28,
+    "bingoBoardId": 2,
+    "position": 4,
+    "exerciseId": 4,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-01-13"),
+    "endAt": new Date("2023-01-13")
+  },
+  {
+    "id": 29,
+    "bingoBoardId": 2,
+    "position": 5,
+    "exerciseId": 5,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-02-14"),
+    "endAt": new Date("2023-02-14")
+  },
+  {
+    "id": 30,
+    "bingoBoardId": 2,
+    "position": 6,
+    "exerciseId": 6,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-03-16"),
+    "endAt": new Date("2023-03-16")
+  },
+  {
+    "id": 31,
+    "bingoBoardId": 2,
+    "position": 7,
+    "exerciseId": 7,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-03-16"),
+    "endAt": new Date("2023-03-16")
+  },
+  {
+    "id": 32,
+    "bingoBoardId": 2,
+    "position": 8,
+    "exerciseId": 8,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-04-16"),
+    "endAt": new Date("2023-04-16")
+  },
+  {
+    "id": 33,
+    "bingoBoardId": 2,
+    "position": 9,
+    "exerciseId": 9,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-04-17"),
+    "endAt": new Date("2023-04-17")
+  },
+  {
+    "id": 34,
+    "bingoBoardId": 2,
+    "position": 10,
+    "exerciseId": 10,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-04-18"),
+    "endAt": new Date("2023-04-18")
+  },
+  {
+    "id": 35,
+    "bingoBoardId": 2,
+    "position": 11,
+    "exerciseId": 11,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-04-18"),
+    "endAt": new Date("2023-04-18")
+  },
+  {
+    "id": 36,
+    "bingoBoardId": 2,
+    "position": 12,
+    "exerciseId": 12,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-04-18"),
+    "endAt": new Date("2023-04-18")
+  },
+  {
+    "id": 37,
+    "bingoBoardId": 2,
+    "position": 13,
+    "exerciseId": 13,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-04-18"),
+    "endAt": new Date("2023-04-18")
+  },
+  {
+    "id": 38,
+    "bingoBoardId": 2,
+    "position": 14,
+    "exerciseId": 14,
+    "isCompleted": true,
+    "inProgress": false,
+    "createAt": new Date("2023-04-18"),
+    "endAt": new Date("2023-04-18")
+  },
+  {
+    "id": 39,
+    "bingoBoardId": 2,
+    "position": 15,
+    "exerciseId": 15,
+    "isCompleted": false,
+    "inProgress": false,
+    "createAt": new Date("1970-01-01"),
+    "endAt": new Date("1970-01-01")
+  },
+  {
+    "id": 40,
+    "bingoBoardId": 2,
+    "position": 16,
+    "exerciseId": 16,
+    "isCompleted": false,
+    "inProgress": false,
+    "createAt": new Date("1970-01-01"),
+    "endAt": new Date("1970-01-01")
+  },
+  {
+    "id": 41,
+    "bingoBoardId": 2,
+    "position": 17,
+    "exerciseId": 17,
+    "isCompleted": false,
+    "inProgress": false,
+    "createAt": new Date("1970-01-01"),
+    "endAt": new Date("1970-01-01")
+  },
+  {
+    "id": 42,
+    "bingoBoardId": 2,
+    "position": 18,
+    "exerciseId": 18,
+    "isCompleted": false,
+    "inProgress": false,
+    "createAt": new Date("1970-01-01"),
+    "endAt": new Date("1970-01-01")
+  },
+  {
+    "id": 43,
+    "bingoBoardId": 2,
+    "position": 19,
+    "exerciseId": 19,
+    "isCompleted": false,
+    "inProgress": false,
+    "createAt": new Date("1970-01-01"),
+    "endAt": new Date("1970-01-01")
+  },
+  {
+    "id": 44,
+    "bingoBoardId": 2,
+    "position": 20,
+    "exerciseId": 20,
+    "isCompleted": false,
+    "inProgress": false,
+    "createAt": new Date("1970-01-01"),
+    "endAt": new Date("1970-01-01")
+  },
+  {
+    "id": 45,
+    "bingoBoardId": 2,
+    "position": 21,
+    "exerciseId": 21,
+    "isCompleted": false,
+    "inProgress": false,
+    "createAt": new Date("1970-01-01"),
+    "endAt": new Date("1970-01-01")
+  },
+  {
+    "id": 46,
+    "bingoBoardId": 2,
+    "position": 22,
+    "exerciseId": 22,
+    "isCompleted": false,
+    "inProgress": false,
+    "createAt": new Date("1970-01-01"),
+    "endAt": new Date("1970-01-01")
+  },
+  {
+    "id": 47,
+    "bingoBoardId": 2,
+    "position": 23,
+    "exerciseId": 23,
+    "isCompleted": false,
+    "inProgress": false,
+    "createAt": new Date("1970-01-01"),
+    "endAt": new Date("1970-01-01")
+  },
+  {
+    "id": 48,
+    "bingoBoardId": 2,
+    "position": 24,
+    "exerciseId": 24,
+    "isCompleted": false,
+    "inProgress": false,
+    "createAt": new Date("1970-01-01"),
+    "endAt": new Date("1970-01-01")
   }
 ];
 
