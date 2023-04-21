d5965dcfc02b412e76e294517350fd4ba4987972
Richard Miao <richardmiao1645@gmail.com>
2023-04-20 18:07:47 -0700

    Fix progress bar bug

--- skeleton/components/UserXP.js
+++ skeleton/components/UserXP.js
@@ -3,6 +3,7 @@ import styles from '/styles/UserXP.module.css';
  
 export default function UserXP(props) {
   
+  //Shows the xp amount needed to progress to the next level.
   const xpProgress = props.xp >= 1000? props.xp % 1000 : props.xp
   const progress = (xpProgress / 1000) * 100;
   const level = Math.floor(props.xp / 1000) + 1;
@@ -11,14 +12,12 @@ export default function UserXP(props) {
     <div>
       <BaseCard title={props.email}>
         Level {level}
-        XP: {props.xp}
         <div className={styles.progressBar}>
           <div className={styles.progressFill} style={{ width: `${progress}%` }}>
             <div className={styles.progressText}>{1000 - xpProgress} XP until next level</div>
           </div>
         </div>
       </BaseCard>
-      
     </div>
   )
 }
