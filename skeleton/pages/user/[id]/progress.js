6e6c8f8cd71515f488c666f2bed8e3f8d328826d
“ehsan-elgendi” <ehsan.elgendi@gmail.com>
2023-04-24 19:44:10 -0400

    add exercises to progress pg mock data

--- skeleton/pages/user/[id]/progress.js
+++ skeleton/pages/user/[id]/progress.js
@@ -9,33 +9,33 @@ export default function Progress() {
     { 
       id: 1,
       squares: [
-        [1, 2, 3, 4, 5],
-        [6, 7, 8, 9, 10],
-        [11, 12, 13, 14, 15],
-        [16, 17, 18, 19, 20],
-        [21, 22, 23, 24, 25]
+        ["Cardio", "Strength", "Cardio", "Strength", "Cardio"],
+        ["Cardio", "Calisthetics", "Cardio", "Strength", "Strength"],
+        ["Cardio", "Strength", "Free Space", "Cardio", "Strength"],
+        ["Cardio", "Calisthetics", "Strength", "Cardio", "Strength"],
+        ["Cardio", "Cardio", "Calisthetics", "Cardio", "Strength"]
       ],
       completed: true
     },
     { 
       id: 2,
       squares: [
-        [1, 2, 3, 4, 5],
-        [6, 7, 8, 9, 10],
-        [11, 12, 13, 14, 15],
-        [16, 17, 18, 19, 20],
-        [21, 22, 23, 24, 25]
+        ["Cardio", "Strength", "Cardio", "Strength", "Cardio"],
+        ["Cardio", "Calisthetics", "Cardio", "Strength", "Strength"],
+        ["Cardio", "Strength", "Free Space", "Cardio", "Strength"],
+        ["Cardio", "Calisthetics", "Strength", "Cardio", "Strength"],
+        ["Cardio", "Cardio", "Calisthetics", "Cardio", "Strength"]
       ],
       completed: true
     },
     { 
       id: 3,
       squares: [
-        [1, 2, 3, 4, 5],
-        [6, 7, 8, 9, 10],
-        [11, 12, 13, 14, 15],
-        [16, 17, 18, 19, 20],
-        [21, 22, 23, 24, 25]
+        ["Cardio", "Strength", "Cardio", "Strength", "Cardio"],
+        ["Cardio", "Calisthetics", "Cardio", "Strength", "Strength"],
+        ["Cardio", "Strength", "Free Space", "Cardio", "Strength"],
+        ["Cardio", "Calisthetics", "Strength", "Cardio", "Strength"],
+        ["Cardio", "Cardio", "Calisthetics", "Cardio", "Strength"]
       ],
       completed: true
     }
