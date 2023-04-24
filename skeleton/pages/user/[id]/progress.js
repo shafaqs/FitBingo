49ee8335ed6e94c7b1a717f1bfeb1c632f630a1e
“ehsan-elgendi” <ehsan.elgendi@gmail.com>
2023-04-24 19:16:43 -0400

    add progress page using mock data

--- skeleton/pages/user/[id]/progress.js
+++ skeleton/pages/user/[id]/progress.js
@@ -1,16 +1,46 @@
 import * as React from "react";
 import Head from 'next/head';
-import {
-  Grid,
-  Button,
-  Box,
-  Card,
-  CardContent,
-  Typography,
-} from "@mui/material";
+import { Grid } from "@mui/material";
 import BaseCard from "@/src/components/baseCard/BaseCard";
+import { useState } from 'react';
 
 export default function Progress() {
+  const [boards, setBoards] = useState([
+    { 
+      id: 1,
+      squares: [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25]
+      ],
+      completed: true
+    },
+    { 
+      id: 2,
+      squares: [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25]
+      ],
+      completed: true
+    },
+    { 
+      id: 3,
+      squares: [
+        [1, 2, 3, 4, 5],
+        [6, 7, 8, 9, 10],
+        [11, 12, 13, 14, 15],
+        [16, 17, 18, 19, 20],
+        [21, 22, 23, 24, 25]
+      ],
+      completed: true
+    }
+  ]);
+
   return (
     <Grid container spacing={0}>
       <Head>
@@ -18,13 +48,33 @@ export default function Progress() {
       </Head>
       {/* ------------------------- row 1 ------------------------- */}
       <Grid item xs={12} lg={12}>
-        <BaseCard title="Current Bingo Board">
-        
-        </BaseCard>
-      </Grid>
-      <Grid item xs={12} lg={12}>
         <BaseCard title="Completed Bingo Boards">
-        
+        <div style={{ display: 'flex' }}>
+      {boards.map(board => (
+        <div key={board.id} style={{ marginRight: '20px' }}>
+          <h4>Board {board.id}</h4>
+          <table>
+            <tbody>
+              {board.squares.map((row, rowIndex) => (
+                <tr key={rowIndex}>
+                  {row.map((square, colIndex) => (
+                    <td
+                      key={colIndex}
+                      style={{ 
+                        backgroundColor: board.completed ? '#03c9d7' : 'white', 
+                        color: board.completed ? 'white' : 'black'
+                      }}
+                    >
+                      {square}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ))}
+    </div>
         </BaseCard>
       </Grid>
     </Grid>
