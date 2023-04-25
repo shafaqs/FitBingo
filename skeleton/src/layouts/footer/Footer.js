a5dd56765663671cdf3a4061f7bf8ec963cca2bc
Sheri Lu <sherisherilu@gmail.com>
2023-04-25 09:25:00 -0700

    Edit footer

--- skeleton/src/layouts/footer/Footer.js
+++ skeleton/src/layouts/footer/Footer.js
@@ -5,10 +5,7 @@ const Footer = () => {
   return (
     <Box sx={{ p: 3, textAlign: "center" }}>
       <Typography>
-        © 2023 All rights reserved by{" "}
-        {/* <Link href="https://www.wrappixel.com">
-          <a>Wrappixel.com</a>
-        </Link>{" "} */}
+        © 2023 All rights reserved
       </Typography>
     </Box>
   );
